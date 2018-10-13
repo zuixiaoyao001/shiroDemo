9d66f9dd62659b3b650e67ee7701d3b7a1c6cdb3
lcy <1248375279@qq.com>
2018-10-13 21:08:40 +0800

    进行java是否认证单元测试

--- src/main/java/com/zxy/demo/javashiro/JavaShiro.java
+++ src/main/java/com/zxy/demo/javashiro/JavaShiro.java
@@ -1,5 +1,11 @@
 package com.zxy.demo.javashiro;
 
+/**
+ * @Title JavaShiro.java
+ * @author 醉逍遥
+ * @date 2018年10月13日
+ * @version 1.0
+ */
 public class JavaShiro {
 
 }
--- src/test/java/com/zxy/demo/JavaShiroTest.java
+++ src/test/java/com/zxy/demo/JavaShiroTest.java
@@ -1,10 +1,42 @@
 package com.zxy.demo;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @Title JavaShiroTest.java
+ * @author 醉逍遥
+ * @date 2018年10月13日
+ * @version 1.0
+ */
 public class JavaShiroTest extends ApplicationTests{
+//	4.指定realm，并在realm中添加一个认证数据来进行测试使用
+	SimpleAccountRealm simpleAccountRealm = new SimpleAccountRealm();
+	@Before
+	public void add() {
+		simpleAccountRealm.addAccount("张三", "123456");
+	}
+	/**
+	 * 创建securityManager，主题提交认证，
+	 * securityManager认证，realm与数据源交互做ren
+	 */
 	@Test
 	public void test() {
-		
+		//1.构建securityManager环境,添加好数据源的realm
+		DefaultSecurityManager securityManager = new DefaultSecurityManager();
+		securityManager.setRealm(simpleAccountRealm);
+		//2.提交主体认证请求
+		SecurityUtils.setSecurityManager(securityManager);
+		Subject subject = SecurityUtils.getSubject();
+		//3.主题发送认证请求前需要带上token的认证数据,同时也需要制定realm,返回去指定realm
+		UsernamePasswordToken token = new UsernamePasswordToken("张三", "123456");
+		subject.login(token);
+		System.out.println("是否认证："+subject.isAuthenticated());
 	}
 }
