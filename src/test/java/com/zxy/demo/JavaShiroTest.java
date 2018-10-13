983b7aa5c15c770e3e6a1ea950a7a05cc6d88fa3
lcy <1248375279@qq.com>
2018-10-13 21:47:55 +0800

    权限认证校验角色单元测试

--- src/test/java/com/zxy/demo/JavaShiroTest.java
+++ src/test/java/com/zxy/demo/JavaShiroTest.java
@@ -16,18 +16,20 @@ import org.junit.Test;
  * @version 1.0
  */
 public class JavaShiroTest extends ApplicationTests{
-//	4.指定realm，并在realm中添加一个认证数据来进行测试使用
+	/**
+	 * 4.指定realm，并在realm中添加一个认证数据来进行测试使用
+	 */
 	SimpleAccountRealm simpleAccountRealm = new SimpleAccountRealm();
 	@Before
 	public void add() {
-		simpleAccountRealm.addAccount("张三", "123456");
+		simpleAccountRealm.addAccount("张三", "123456","admin","user");
 	}
 	/**
 	 * 创建securityManager，主题提交认证，
 	 * securityManager认证，realm与数据源交互做ren
 	 */
 	@Test
-	public void test() {
+	public void testAuthenticate() {
 		//1.构建securityManager环境,添加好数据源的realm
 		DefaultSecurityManager securityManager = new DefaultSecurityManager();
 		securityManager.setRealm(simpleAccountRealm);
@@ -37,6 +39,11 @@ public class JavaShiroTest extends ApplicationTests{
 		//3.主题发送认证请求前需要带上token的认证数据,同时也需要制定realm,返回去指定realm
 		UsernamePasswordToken token = new UsernamePasswordToken("张三", "123456");
 		subject.login(token);
+		System.out.println("是否认证："+subject.isAuthenticated());
+//	     校验是否授予了管理员角色权限
+		subject.checkRoles("admin","user");
+//		退出
+		subject.logout();
 		System.out.println("是否认证："+subject.isAuthenticated());
 	}
 }
